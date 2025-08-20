df1862839eef4fbc0ebe3c737280ab9c53367a4c
Kalana Dharmarathne <kalanaeshan07@gmail.com>
2025-08-20 21:18:52 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import GitHubStats from "./components/GitHubStats";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <GitHubStats />
       <Contact />
     </>
   );
